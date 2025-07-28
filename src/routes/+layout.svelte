d322c5e640e3acb76e76f0ec6218ca7c4e6b761f
Ukasyah <hi.ukasyah@gmail.com>
2025-07-28 19:12:54 +0700

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { Provider } from '@sevria/ui';
+	import '@sevria/ui/styles.css';
+
+	let { children } = $props();
+</script>
+
+<Provider>
+	{@render children()}
+</Provider>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Container } from '@sevria/ui';
+</script>
+
+<div class="flex h-screen flex-col items-center justify-center text-center">
+	<h1 class="text-foreground-brand text-4xl font-semibold">Welcome to Sevria app</h1>
+
+	<Container class="mt-8" size="sm">
+		<p>
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam earum inventore asperiores
+			atque dolorum aspernatur nesciunt maxime? Doloribus, nobis. Facilis accusamus fugit sequi
+			incidunt? Cum iusto eum et nam recusandae.
+		</p>
+	</Container>
+</div>
